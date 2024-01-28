1207090d4f3e5f2aee04c9ef2246aaf3edfd7970
duckistbabu <157822150+duckistbabu@users.noreply.github.com>
2024-01-28 23:45:33 +0600

    Add files via upload

---
+++ links.js
@@ -0,0 +1,4 @@
+var link1 = "https://smrturl.co/o/548074/53177516?s1=";
+var link2 = "https://smrturl.co/o/548074/53177516?s1=";
+var link3 = "https://smrturl.co/o/548074/53177516?s1=";
+var link4 = "https://smrturl.co/o/548074/53177516?s1=";
---
+++ salkjshkdakjsdl.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("link1")[0].href = link1;
+document.getElementsByClassName("link2")[0].href = link2;
+document.getElementsByClassName("link3")[0].href = link3;
+document.getElementsByClassName("link4")[0].href = link3;

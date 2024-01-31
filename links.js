0612cdd282db38f021794a4d9c0b16bb726b57aa
duckistbabu <157822150+duckistbabu@users.noreply.github.com>
2024-01-31 18:13:13 +0600

    Update links.js

--- links.js
+++ links.js
@@ -1,4 +1,1 @@
-var link1 = "https://smrturl.co/o/548074/53177516?s1=";
-var link2 = "https://smrturl.co/o/548074/53177516?s1=";
-var link3 = "https://smrturl.co/o/548074/53177516?s1=";
-var link4 = "https://smrturl.co/o/548074/53177516?s1=";
+var cpalink = "https://smrturl.co/o/548074/53177516?s1=";

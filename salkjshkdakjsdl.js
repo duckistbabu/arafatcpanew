2af116054f9042b8af10c2c340ddf40c740d41cf
duckistbabu <157822150+duckistbabu@users.noreply.github.com>
2024-01-31 18:13:24 +0600

    Update salkjshkdakjsdl.js

--- salkjshkdakjsdl.js
+++ salkjshkdakjsdl.js
@@ -1,4 +1,1 @@
-document.getElementsByClassName("link1")[0].href = link1;
-document.getElementsByClassName("link2")[0].href = link2;
-document.getElementsByClassName("link3")[0].href = link3;
-document.getElementsByClassName("link4")[0].href = link3;
+document.getElementsByClassName("cpalink")[0].href = cpalink;
